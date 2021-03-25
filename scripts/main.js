6ac31bbbb5d01ce35c6cf74f20fb98b3ee3a8ccf
Henry <henry3411126@gmail.com>
2021-03-25 13:57:14 +0800

    1.5

--- scripts/main.js
+++ scripts/main.js
@@ -7,7 +7,7 @@ function changelist1()
       switch(document.data.abroad.value)
       {
             case "YES":
-            document.getElementById("abroad_q1").innerHTML='<ol><li>入境日期 <INPUT type="date" name="e_day"><br></li><li>最近入境台灣之來源地區 <INPUT type="text" name="source"><br></li><li>搭乘班機航空公司與編號 <INPUT type="text" name="flight"><br></li></ol>'
+            document.getElementById("abroad_q1").innerHTML='<ol><li>入境日期 <INPUT type="date" name="e_day" placeholder="西元    年-月-日" class="mydate"><br></li><li>最近入境台灣之來源地區 <INPUT type="text" name="source"><br></li><li>搭乘班機航空公司與編號 <INPUT type="text" name="flight"><br></li></ol>'
             break;
             case "NO":
             document.getElementById("abroad_q1").innerHTML="";
@@ -20,7 +20,7 @@ function changelist2()
       switch(document.data.contact_1.value)
       {
             case "YES":
-            document.getElementById("abroad_q2").innerHTML='<ol><li>出國者與您的關係是？<br><INPUT type="radio" name="abroad_p" value="家人">家人<br><INPUT type="radio" name="abroad_p" value="親戚">親戚<br><INPUT type="radio" name="abroad_p" value="朋友">朋友<br><INPUT type="radio" name="abroad_p" value="其他">其他 <INPUT type="text" name="abroad_person"><br></li><li>入境時間？<INPUT type="date" name="fe_time"></li><li>管理措施？<br><INPUT type="radio" name="manage" value="居家隔離">居家隔離<br><INPUT type="radio" name="manage" value="居家檢疫">居家檢疫<br><INPUT type="radio" name="manage" value="自主健康管理">自主健康管理<br><INPUT type="radio" name="manage" value="入院治療">入院治療<br><INPUT type="radio" name="manage" value="其他">其他<br></li><li>管理措施開始時間？<INPUT type="date" name="manage_start"><br></li><li>管理措施結束時間？<INPUT type="date" name="manage_end"><br></li><li>出國者是否有症狀？（有症狀者請註明例如:發燒、咳嗽 ）<INPUT type="text" name="abroad_ill"><br></li></ol>'
+            document.getElementById("abroad_q2").innerHTML='<ol><li>出國者與您的關係是？<br><INPUT type="radio" name="abroad_p" value="家人">家人<br><INPUT type="radio" name="abroad_p" value="親戚">親戚<br><INPUT type="radio" name="abroad_p" value="朋友">朋友<br><INPUT type="radio" name="abroad_p" value="其他">其他 <INPUT type="text" name="abroad_person"><br></li><li>入境時間？<INPUT type="date" name="fe_time" placeholder="西元    年-月-日" class="mydate"></li><li>管理措施？<br><INPUT type="radio" name="manage" value="居家隔離">居家隔離<br><INPUT type="radio" name="manage" value="居家檢疫">居家檢疫<br><INPUT type="radio" name="manage" value="自主健康管理">自主健康管理<br><INPUT type="radio" name="manage" value="入院治療">入院治療<br><INPUT type="radio" name="manage" value="其他">其他<br></li><li>管理措施開始時間？<INPUT type="date" name="manage_start" placeholder="西元    年-月-日" class="mydate"><br></li><li>管理措施結束時間？<INPUT type="date" name="manage_end" placeholder="西元    年-月-日" class="mydate"><br></li><li>出國者是否有症狀？（有症狀者請註明例如:發燒、咳嗽 ）<INPUT type="text" name="abroad_ill"><br></li></ol>'
             break;
             case "NO":
             document.getElementById("abroad_q2").innerHTML="";
