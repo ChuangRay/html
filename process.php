593cfed00ad78205689c8e4473cb2cf9ff627ccf
Henry <henry3411126@gmail.com>
2021-03-25 15:23:44 +0800

    1.4

--- process.php
+++ process.php
@@ -3,7 +3,7 @@ header("Content-Type:text/html; charset=utf-8");
   //建立連線：
   $link = mysql_pconnect("192.168.2.39", "root", "estinet"); //$link宣告連線，mysql_pconnet("主機名稱","資料庫帳號","資料庫密碼");
   //選擇資料庫：
-  mysql_select_db("mydb") or die("無法選擇資料庫"); // 選擇資料庫：mysql_select_db("資料庫名稱") or die ("失敗顯示字");
+  mysql_select_db("estinet") or die("無法選擇資料庫"); // 選擇資料庫：mysql_select_db("資料庫名稱") or die ("失敗顯示字");
   //二、執行SQL語法
   $name = mysql_real_escape_string($_POST['name']);
   $phone = mysql_real_escape_string($_POST['phone']);
