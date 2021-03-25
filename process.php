5e7074f47b8375617f70555f54c510553a3ac340
Henry <henry3411126@gmail.com>
2021-03-25 15:26:31 +0800

    1.5

--- process.php
+++ process.php
@@ -3,13 +3,13 @@ header("Content-Type:text/html; charset=utf-8");
   //建立連線：
   $link = mysql_pconnect("192.168.2.39", "root", "estinet"); //$link宣告連線，mysql_pconnet("主機名稱","資料庫帳號","資料庫密碼");
   //選擇資料庫：
-  mysql_select_db("estinet") or die("無法選擇資料庫"); // 選擇資料庫：mysql_select_db("資料庫名稱") or die ("失敗顯示字");
+  mysql_select_db("mydb") or die("無法選擇資料庫"); // 選擇資料庫：mysql_select_db("資料庫名稱") or die ("失敗顯示字");
   //二、執行SQL語法
   $name = mysql_real_escape_string($_POST['name']);
   $phone = mysql_real_escape_string($_POST['phone']);
   //在html的name上面寫什麼這邊就宣告什麼，後面POST是資料庫欄位
   // 建立SQL語法，使用$query
-    $query = "INSERT INTO test (id) VALUES ('$name')";
+    $query = "INSERT INTO personal info (name) VALUES ('$name')";
 //$query = "insert into 資料表名稱(資料表欄位,資料表欄位2) values ('輸入值1','輸入值2'); 由於html欄位name=account，這邊就也是$account（已宣告過）
   //送出SQL語法到資料庫系統
   mysql_query($query) or die("無法送出" . mysql_error( ));
